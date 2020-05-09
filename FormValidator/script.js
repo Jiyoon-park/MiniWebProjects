76fd54683a84824e80f38194ed1540ff32eb37b5
박지윤 <soundpjy1205@gmail.com>
2020-05-09 14:34:47 +0900

    FormValidator | basic

--- FormValidator/script.js
+++ FormValidator/script.js
@@ -19,7 +19,7 @@ function showSuccess(input) {
 function checkPasswordsMatch(input1, input2) {
     if(input1.value !== input2.value ) {
         showError(input2, 'Passwords do not match');
-    } 
+    }
 }
 
 function checkEmail(input) {
