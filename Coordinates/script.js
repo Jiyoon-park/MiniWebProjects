1f633256277958cb71b514a501ccd2df302a739d
Jiyoon-park <soundpjy1205@gmail.com>
2020-12-05 23:44:39 +0900

    Ugrade performance | Coordinates

--- Coordinates/script.js
+++ Coordinates/script.js
@@ -1,20 +1,22 @@
 const horizontal = document.querySelector('.horizontal');
 const vertical = document.querySelector('.vertical');
 const target = document.querySelector('.target');
+const targetRect = target.getBoundingClientRect();
+const targetHalfWidth = targetRect.width / 2;
+const targetHalfHeight = targetRect.height / 2;
 const tag = document.querySelector('.tag');
+
 
 document.addEventListener('mousemove', (e)=> {
     const x = e.clientX;
     const y = e.clientY;
 
-    horizontal.style.top = `${y}px`;
-    vertical.style.left = `${x}px`;
+    horizontal.style.transform = `translateY(${y}px)`;
+    vertical.style.transform = `translateX(${x}px)`;
     
-    target.style.top = `${y}px`;
-    target.style.left = `${x}px`;
+    target.style.transform = `translate(${x-targetHalfWidth}px, ${y-targetHalfHeight}px)`;
 
-    tag.style.top = `${y}px`;
-    tag.style.left = `${x}px`;
+    tag.style.transform = `translate(${x+20}px, ${y+20}px)`;
     tag.innerText = `${x}px, ${y}px`
 })
 
