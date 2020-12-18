514f44cf7317f9876e62ca7a07329b9a56e08ce8
Jiyoon-park <soundpjy1205@gmail.com>
2020-12-18 15:38:41 +0900

    feature | Add stop game function

--- PickCarrotGame/script.js
+++ PickCarrotGame/script.js
@@ -1,20 +1,24 @@
 'use strict';
 
-const CARROT_SIZE = 80;
-const CARROT_COUNT = 5;
-const BUG_COUNT = 5;
-
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
 
 const gameBtn = document.querySelector('.game__button');
 const gameTimer = document.querySelector('.game__timer');
 const gameScore = document.querySelector('.game__score');
+
 const popUp = document.querySelector('.pop-up');
+const popUpText = popUp.querySelector('.pop-up__message');
+const popUpreDo = popUp.querySelector('.pop-up__redo');
+
+const CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+const GAME_DURATION_SEC = 5;
 
 let started = false;
 let score = 0;
-let timer = 0;
+let timer = undefined;
 
 gameBtn.addEventListener('click', () => {
     if (started) {
@@ -22,16 +26,22 @@ gameBtn.addEventListener('click', () => {
     } else {
         startGame();
     }
-    started = !started;
 })
 
 function startGame() {
+    started = true;
     initGame();
     showStopButton();
     showTimerAndScore();
+    startGameTimer();
 }
 
-function stopGame() {}
+function stopGame() {
+    started = false;
+    stopGameTimer();
+    hideGameButton();
+    showPopUpWithText('REPLAY?');
+}
 
 function showStopButton() {
     const icon = gameBtn.querySelector('.fa-play');
@@ -39,15 +49,46 @@ function showStopButton() {
     icon.classList.remove('fa-play');
 }
 
+function hideGameButton() {
+    gameBtn.style.visibility = 'hidden';
+}
+
 function showTimerAndScore() {
     gameTimer.style.visibility = 'visible';
     gameScore.style.visibility = 'visible';
 }
 
+function startGameTimer() {
+    let remainingTimeSec = GAME_DURATION_SEC;
+    updateTimerText(remainingTimeSec);
+    timer = setInterval(()=> {
+        if (remainingTimeSec <= 0 ) {
+            clearInterval(timer);
+            return
+        }
+        updateTimerText(--remainingTimeSec);
+    }, 1000)
+}
+
+function stopGameTimer() {
+    clearInterval(timer);
+}
+
+function updateTimerText(time) {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    gameTimer.innerText = `${minutes}:${seconds}`;
+}
+
+function showPopUpWithText(text) {
+    popUp.classList.remove('pop-up--hide');
+    popUpText.innerText = text;
+}
+
 function initGame() {
     field.innerHTML = '';
+    gameTimer.innerText = timer;
     gameScore.innerText = CARROT_COUNT;
-    // 당근과 벌레 랜덤 배치
     addItem('carrot', CARROT_COUNT, 'img/carrot.png');
     addItem('bug', BUG_COUNT, 'img/bug.png');
 }
